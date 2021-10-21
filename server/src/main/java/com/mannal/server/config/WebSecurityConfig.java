a8fec8dc0c479f06e535f80f8f87b2e6415a6463
류동욱 <udonguk@gmail.com>
2021-10-21 22:10:28 +0900

    + 일단 테스트를 위해서 security는 풀어둠

---
+++ server/src/main/java/com/mannal/server/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.mannal.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 일단은 모든 요청 허용 해둠
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}
