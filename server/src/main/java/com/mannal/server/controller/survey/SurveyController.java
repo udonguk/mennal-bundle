0d0719e5b166c865aa921299471fb80ba6a6ad6e
donguk <udonguk@gmail.com>
2022-03-31 12:35:46 +0900

    feat(BACK): 학습성향 계산 기능

--- server/src/main/java/com/mannal/server/controller/survey/SurveyController.java
+++ server/src/main/java/com/mannal/server/controller/survey/SurveyController.java
@@ -2,16 +2,13 @@ package com.mannal.server.controller.survey;
 
 import com.mannal.server.dto.SurveyResultDto;
 import com.mannal.server.entity.survey.SurveyEntity;
-import com.mannal.server.entity.survey.SurveyItemOptionEntity;
 import com.mannal.server.entity.survey.SurveyItemOptionResultEntity;
-import com.mannal.server.entity.survey.SurveyResultEntity;
+import com.mannal.server.entity.survey.SurveyItemResultEntity;
 import com.mannal.server.service.survey.SurveyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,27 +29,6 @@ public class SurveyController {
         return ResponseEntity.ok(surveyService.findSurvey(UUID.fromString(categoryId)));
     }
 
-//    @PostMapping
-//    public void add(@RequestBody List<UUID> userIds) {
-//        adminService.save(userIds);
-//    }
-
-    @PostMapping
-    @ResponseBody
-    public ResponseEntity<SurveyResultDto> sendSurvey(
-            @RequestBody List<SurveyResultEntity> surveyResultEntityList
-    ){
-
-        UUID requestId = UUID.randomUUID();
-        LocalDateTime currentTime = LocalDateTime.now();
-        surveyResultEntityList.forEach(surveyResult -> {
-            setSurveyResultDefault(requestId, currentTime, surveyResult);
-        });
-
-        surveyService.saveResult(surveyResultEntityList);
-        return ResponseEntity.ok(surveyService.getResult(requestId));
-    }
-
     @PostMapping("/option")
     @ResponseBody
     public ResponseEntity<SurveyResultDto> sendSurveyOptionResult(
@@ -62,16 +38,15 @@ public class SurveyController {
         UUID requestId = UUID.randomUUID();
         LocalDateTime currentTime = LocalDateTime.now();
 
-        surveyItemOptionResultEntityList.forEach(surveyItemOptionResult -> {
+        for (SurveyItemOptionResultEntity surveyItemOptionResult : surveyItemOptionResultEntityList) {
             setSurveyOptionResultDefault(requestId, currentTime, surveyItemOptionResult);
-        });
+        }
 
         surveyService.saveOptionResult(surveyItemOptionResultEntityList);
-//        return ResponseEntity.ok(surveyService.getResult(requestId));
         return ResponseEntity.ok(surveyService.getOptionResult(requestId));
     }
 
-    private void setSurveyResultDefault(UUID requestId, LocalDateTime currentTime, SurveyResultEntity surveyResult) {
+    private void setSurveyResultDefault(UUID requestId, LocalDateTime currentTime, SurveyItemResultEntity surveyResult) {
         surveyResult.setRequestId(requestId);
         surveyResult.setUseYn("Y");
         surveyResult.setRegDt(currentTime);
