142816bf1e7793a545dffae07f83eb32a50e8cc5
donguk <udonguk@gmail.com>
2022-06-29 19:58:20 +0900

    - 포트 변경

--- web/react-app/src/config/axios/api.js
+++ web/react-app/src/config/axios/api.js
@@ -6,14 +6,12 @@ const axiosToApi = axios.create({
   // baseURL: 'http://www.mennal.co.kr/api',
   // baseURL: 'http://192.168.30.80/api',
   // baseURL: 'http://106.244.187.76:9004/api',
-  baseURL: 'http://localhost/api',
+  baseURL: 'http://localhost:85/api',
+  // baseURL: 'http://192.168.30.80:85/api',
   headers: {'X-Custom-Header': 'foobar'}
 })
 
-
-// axiosToApi.defaults.xsrfCookieName = 'csrftoken';
 axiosToApi.defaults.xsrfCookieName = 'XSRF-TOKEN';
-// axiosToApi.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 axiosToApi.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
 
 axiosToApi.interceptors.request.use(
